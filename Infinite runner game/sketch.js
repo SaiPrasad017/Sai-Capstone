bbe2ccf05ead43b966401b2f2b53d2fa1924c79f
SaiPrasad017 <3201237@spsd.sk.ca>
2024-12-02 15:19:46 -0600

    add a background and an object to my ground
    also found the sprites i will be using in the future. improved on the ground

--- Infinite runner game/sketch.js
+++ Infinite runner game/sketch.js
@@ -2,9 +2,12 @@
 // Sai Prasad
 // 11/28/2024
 
+//too add list
+//seconed background to move from oppsite direction to get rid of any blind spots
+
 let easyMode = 0;
-// let normalMode = 1;
-// let hardMode = 2;
+let normalMode = 10;
+let hardMode = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,8 +17,6 @@ function setup() {
 function draw() {
   background(220);
   ground.action();
-  fill(1,255,255);
-  //rect(width/2,height*2/3 - 50,50,50); hight for the charater
 }
 
 class Ground{
@@ -23,11 +24,22 @@ class Ground{
     this.xPosition = windowWidth;
     this.yPosition = yPosition;
     this.speed = speed;
+    this.floorX = windowWidth;
+    this.sky = windowWidth;
   }
 
   display(){
+    //sky
+    fill(0,100,255);
+    square(this.sky,this.yPosition/3, windowHeight* windowWidth);
+
     fill(255,255,255);
     rect(this.xPosition,this.yPosition,width,500);
+
+    //banner
+    fill(255,0,0);
+    square(this.floorX,this.yPosition + 25, 25, 25);
+
   }
 
   move(){
@@ -43,10 +55,32 @@ class Ground{
 
     this.xPosition = this.xPosition - (this.speed + easyMode); //tempary line remove later
 
+    //floor design move
+    this.floorX = this.floorX - (this.speed + easyMode);
+
+    //BACKGROUND MOVING
+    this.sky = this.sky - (this.speed - 2);
+
+    //floor moving
     if(this.xPosition < 0){
       this.xPosition = 0;
     }
-    
+
+    //making the floor design loop
+    if(this.floorX < 0){
+      this.floorX += width;
+    }
+    if(this.floorX > width){
+      this.floorX -= width;
+    }
+
+    //making sky move
+    if(this.sky < 0){
+      this.sky += width;
+    }
+    if(this.sky > width){
+      this.sky -= width;
+    }
   }
 
   action(){
@@ -54,3 +88,16 @@ class Ground{
     this.move();
   }
 }
+
+
+
+// class charater{
+//   constructor(){
+//   }
+
+//   display(){
+//     fill(1,255,255);
+//     rect(width/2,height*2/3 - 50,50,50);//place holder
+//   }
+
+// }
